6f105ff6620fa4c448e5d797bb1d40d576b86bf2
Animesh <animesh0404@gmail.com>
2026-06-22 15:32:31 +0530

    chore: add eslint and prettier config

--- practice/server.js
+++ practice/server.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello Smart Library");
+  res.end('Hello Smart Library');
 });
 
 server.listen(3000, () => {
-  console.log("Server running");
+  console.log('Server running');
 });
